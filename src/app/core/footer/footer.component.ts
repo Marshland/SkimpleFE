f92b95ee55012f37e4e6a3d18a93f053083b378c
Marshland <francesco.romano94@live.com>
2018-11-28 12:40:23 +0100

    add auto version

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VERSION } from '../../../environments/version';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   currentYear: number;
+  version = VERSION;
 
   constructor() {}
 
---
+++ src/environments/version.ts
@@ -0,0 +1,14 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "668ee9c-dirty",
+    "hash": "668ee9c",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "668ee9c-dirty",
+    "semverString": null,
+    "version": "0.0.0"
+};
+/* tslint:enable */

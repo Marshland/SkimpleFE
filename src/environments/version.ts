a9cc1b0fe60b4c4a5475dab0758e63e06026d2dd
Marshland <francesco.romano94@live.com>
2018-11-28 13:04:09 +0100

    change version

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,14 +1,14 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "f92b95e-dirty",
-    "hash": "f92b95e",
+    "dirty": false,
+    "raw": "15c6673",
+    "hash": "15c6673",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "f92b95e-dirty",
+    "suffix": "15c6673",
     "semverString": null,
-    "version": "1.0.0"
+    "version": "0.0.1"
 };
 /* tslint:enable */
